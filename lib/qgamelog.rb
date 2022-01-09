f79567be84ed4d312ee78fdd652d5159b56f36a9
matheus <mbizzoni@repay.com>
2022-01-09 11:00:30 -0300

    first commit

---
+++ lib/qgamelog.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "qgamelog/version"
+
+module Qgamelog
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/qgamelog_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class QgamelogTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Qgamelog::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
